4d8ac70264849c6a8c26ea2d136f050de615c2f2
Pedro Paião <devpedropaiao@gmail.com>
2021-10-30 17:22:21 -0300

    [FIX] db on heroku

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,12 +4,13 @@ const dbConfig = require('../config/database.ts')
 var connection: Sequelize;
 
 if (process.env.DATABASE_URL != null) {
-  connection = new Sequelizes(process.env.DATABASE_URL, {
+  connection = new Sequelizes(`${process.env.DATABASE_URI}?sslmode=require`, {
+      url: process.env.DATABASE_URI,
       dialect: 'postgres',
       protocol: 'postgres',
       dialectOptions: {
           ssl: true,
-          rejectUnauthorized: false
+          rejectUnauthorized: false, // very important
       },
       database: 'criticism_app_production',
       define: {
