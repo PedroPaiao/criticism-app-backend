a2c1a8ba936caa4ed03679f5b742d583d2f4eba7
Pedro Paião <devpedropaiao@gmail.com>
2021-10-30 16:37:02 -0300

    [FIX] Deploy

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './routes'
 
-const app = express();
+const app = express()
+const PORT : string|number = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(routes);
+app.use("*",(req, res) =>{
+    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+});
 
-app.listen(process.env.PORT || 3333);
+app.listen(PORT,() => console.log(`hosting @${PORT}`));
