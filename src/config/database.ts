497fa3a382dc62ca6ac57067ad6c240f7ace8e7b
Pedro Paião <devpedropaiao@gmail.com>
2021-10-30 16:50:57 -0300

    [FIX] db on heroku

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,15 @@
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'criticism_app_development',
-    define: {
-        timestamps: true,
-        underscored: true
-    }
+  dialect: 'postgres',
+  host: 'localhost',
+  port: process.env.DATABASE_URL || 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'criticism_app_development',
+  ssl: {
+    rejectUnauthorized: false
+  },
+  define: {
+    timestamps: true,
+    underscored: true
+  }
 }
