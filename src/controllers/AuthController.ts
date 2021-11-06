8ca6b239eae32b6e92feeb4dc824085183ec3911
vieiramanda <amandavc27@gmail.com>
2021-11-06 19:07:54 -0400

    [NEW] users authentication

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,26 +2,41 @@ import { Request, Response } from "express";
 import User from '../models/User';
 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 class AuthContoller {
-
+  
   async handleLogin(req: Request, res: Response) {
     const { email, password } = req.body;
     
     if (email == null || password == null) {
-      return res.status(400).json({ message: 'Username and password are required!' })    
+      return res.status(400).json({ message: 'Username and password are required!' });
     }
 
-    const user = await User.findOne({ where: { email:  email} })
+    const user = await User.findOne({ where: { email:  email} });
     
     if (user == null) {
-      return res.status(401).send({ message: 'Unathorized user' })
+      return res.status(401).send({ message: 'Unathorized user' });
     }
 
-    const match = await bcrypt.compare(password, user.get('password'))
+    const match = await bcrypt.compare(password, user.get('password'));
     if (match) {
       // create JWTs
-      return res.json({ message : `Succes! User ${user.get('email')} is logged in.` })
+      const acessToken = jwt.sign(
+        { "userEmail": user.get('email') },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '60s' }
+      );
+      
+      const refreshToken = jwt.sign(
+        { "userEmail": user.get('email') },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '1d' }
+      );
+
+      res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24*60*60*1000} );
+      return res.json({ message : `Succes! User ${user.get('email')} is logged in.`, acessToken });
     }
     
     return res.status(401).send({ message: 'Unathorized user. Credentials dont match!' })
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
-import express, { response } from 'express';
+import express, { Request, Response } from 'express';
+
 import AuthContoller from './controllers/AuthController';
 import MoviesController from './controllers/MoviesController';
 import UsersController from './controllers/UsersController';
+import verifyJWT from './middlewares/verifyJWT';
 
 const routes = express.Router();
 
@@ -9,7 +11,7 @@ const moviesController = new MoviesController;
 const usersController = new UsersController;
 const authController = new AuthContoller;
 
-routes.get('/', (request, response) => {
+routes.get('/', (request: Request, response: Response) => {
   response.send('Hello World!')
 });
 
@@ -18,13 +20,14 @@ routes.get('/movies', moviesController.index)
 routes.delete('/movies/:id', moviesController.delete)
 routes.put('/movies/:id', moviesController.update)
 
-routes.get('/users', usersController.index)
+routes.get('/users', verifyJWT, usersController.index)
 routes.get('/users/:id', usersController.show)
 routes.post('/users', usersController.store)
 routes.put('/users/:id', usersController.update)
 routes.delete('/users/:id', usersController.delete)
 
 routes.post('/users/login', authController.handleLogin)
+/*logout?*/
 
 
 
